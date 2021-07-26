5a08619b917ab66a1c99ab2055724d93d6cb3fe4
Yishmael Henderson <yishmael.henderson@gmail.com>
2021-07-26 09:46:17 -0500

    working on pulling movies from database

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
 "use strict";
 
-fetch('https://rain-wealthy-teacher.glitch.me/movies').then( response => {
-    response.json().then( movies => {
-        console.log(movies);
+
+//movies list in a variable
+
+const movieSelection = document.getElementById("movies");
+
+//function to pull movies from database
+
+function selectMovie(){
+    const movieSelector = document.querySelector("form");
+    fetch('https://rain-wealthy-teacher.glitch.me/movies').then( response => {
+        response.json().then( response => {
+            appendMovie(response);
+        });
     });
-});
+}
+
