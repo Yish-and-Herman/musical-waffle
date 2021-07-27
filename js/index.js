2d229a5ddc81698c0305e3e70bef212a19e02032
Herman Bimikwe <bimikwe.herman@gmail.com>
2021-07-27 15:40:49 -0500

    Loading icon added

--- js/index.js
+++ js/index.js
@@ -1,33 +1,33 @@
 "use strict";
-
+$(document).ready(function () {
 
 //movies list in a variable
-const url = 'https://rain-wealthy-teacher.glitch.me/movies'
-const movieSelection = document.getElementById("movies");
+    const url = 'https://rain-wealthy-teacher.glitch.me/movies'
+    const movieSelection = document.getElementById("movies");
 
 
-function AJAX(url, method = "GET", data) {
-    const options = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    };
-    return fetch(url, options)
-        .then(res => res.json())
-        .then(responseData => responseData)
-        .catch(err => err)
-}
+    function AJAX(url, method = "GET", data) {
+        const options = {
+            method: method,
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        };
+        return fetch(url, options)
+            .then(res => res.json())
+            .then(responseData => responseData)
+            .catch(err => err)
+    }
 
-function getAllMovies() {
-    AJAX(url).then(responseData => {
-        console.log(responseData)
-        responseData.forEach(function (movie) {
-            // let html = "";
-            $('#movies').append(
-                // html +=
-                `<div class="card" style="width: 18rem;">
+    function getAllMovies() {
+        AJAX(url).then(responseData => {
+            console.log(responseData)
+            responseData.forEach(function (movie) {
+                // let html = "";
+                $('#movies').append(
+                    // html +=
+                    `<div class="card" style="width: 18rem;">
   <img src=${movie.poster} class="card-img-top" alt="...">
   <div class="card-body">
    <p class="card-text">Movie title: ${movie.title}</p>
@@ -38,13 +38,14 @@ function getAllMovies() {
 
   </div>
 </div>`)
-        })
-        addEventListeners()
-    });
-}
+            })
+            addEventListeners()
+        });
 
+            $('.spinner-border').hide();
+    }
 
-getAllMovies()
+    getAllMovies()
 
 // function getOneMovie(id) {
 //     AJAX(`${url}/${id}`).then(responseData => console.log(responseData));
@@ -53,45 +54,44 @@ getAllMovies()
 // getOneMovie(2)
 
 
+    function addEventListeners() {
 
-function addEventListeners() {
-
-    $('.delete-button').click(function () {
-        const idToDelete = $(this).attr("data-id")
-        console.log(idToDelete);
-        AJAX(`${url}/${idToDelete}`, "DELETE").then(responseData => console.log(responseData));
-        getAllMovies()
-    });
-    $('.edit-button').click(function () {
-        const idToEdit = $(this).attr("data-id")
-        $('#myModal').modal('show')
-        $('#saveChanges').click(function () {
+        $('.delete-button').click(function () {
+            const idToDelete = $(this).attr("data-id")
+            console.log(idToDelete);
+            AJAX(`${url}/${idToDelete}`, "DELETE").then(responseData => console.log(responseData));
+            getAllMovies()
+        });
+        $('.edit-button').click(function () {
+            const idToEdit = $(this).attr("data-id")
+            $('#myModal').modal('show')
+            $('#saveChanges').click(function () {
+            })
         })
-    })
-}
+    }
 
-function updateMovies(id) {
-    AJAX(`${url}/${id}`, "PATCH", {plot: "Yep"}).then(responseData => console.log(responseData));
+    function updateMovies(id) {
+        AJAX(`${url}/${id}`, "PATCH", {plot: "Yep"}).then(responseData => console.log(responseData));
 
-}
+    }
 
 // updateMovies(30)
 
-function addMovie() {
-    const movieToAdd = {
-        title: $('#movie-title').val(),
-        rating: $('#rating').val(),
-    };
-    console.log(movieToAdd)
+    function addMovie() {
+        const movieToAdd = {
+            title: $('#movie-title').val(),
+            rating: $('#rating').val(),
+        };
+        console.log(movieToAdd)
 
-    AJAX(url, "POST", movieToAdd).then(responseData => console.log(responseData));
-}
+        AJAX(url, "POST", movieToAdd).then(responseData => console.log(responseData));
+    }
 
-$('#submit-movie').click(function (event) {
-    event.preventDefault()
-    addMovie()
-});
-
+    $('#submit-movie').click(function (event) {
+        event.preventDefault()
+        addMovie()
+    });
+})
 
 //div w/ id movies
 //create html
